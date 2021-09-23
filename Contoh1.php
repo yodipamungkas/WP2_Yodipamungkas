261f98190989c2bb75557cde32230284f22b96bb
yodipamungkas <yodipamungkas6639@gmail.com>
2021-09-23 21:26:08 +0700

    upload file CI3 dan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+class Contoh1 extends CI_Controller
+{
+	public function_index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Yodi Pamungkas
+			Saya tinggal di daerah cibanteng proyek
+			olahraga saya yang saya sukai adalah 
+			Bulutangkis";
+	}
+}
