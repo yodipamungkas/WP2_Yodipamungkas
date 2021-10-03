b30c5306639b2de5c2672165057ec1c148fb4c29
yodipamungkas <yodipamungkas6639@gmail.com>
2021-10-03 11:19:48 +0700

    view

---
+++ application/views/view_latihan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan1</title>
+</head>
+<body>
+	Hallo teman-teman Saya yodi ayok belajar web programming <br>
+	Nilai 1 = <?= $nilai1;?>
+	Nilai 2 = <?= $nilai2;?>
+	ini hasil penjumlahan dari metode model yaitu <?= $nilai1 . " + ". $nilai2. " = ".$hasil; ?>
+</body>
+</html> 
