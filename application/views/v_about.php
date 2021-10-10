57e448040634035dbb63983e527cdf941c804446
yodipamungkas <yodipamungkas6639@gmail.com>
2021-10-10 20:13:04 +0700

    membuat template sederhana dan dinamis

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan      : Yodi </li>
+        <li>Nama Belakang   : Pamungkas
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> kp.pasar rebo Rt 01/08 Desa cihideung udik kec.ciampea Kab.BOGOR</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>BOGOR</li>
+    </ul>
+
+    <h4>Olaha Raga Favorite</h4>
+    <ul type="square">
+        <li>BULU TANGKIS</li>
+        <li>MUSIK</li>
+    </ul>
+</section>
